47e728934f113b1b69d876e6d036f09ae96f5d09
Pavel Nishcharetau <alzarlion@gmail.com>
2018-02-28 04:28:32 +0300

    Task Money-Exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var h = 50, q = 25, d = 10, n = 5, p = 1, remainder, result = {};
+    var ch, cq, cd, cn ,cp;
+
+    if(currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    } else if(currency <= 0) {
+        return {};
+    } else {
+        remainder = currency;
+
+        if (remainder >= h) {
+            ch = Math.floor(remainder / h);
+            result["H"] = ch;
+            h *= ch;
+            remainder -= h;
+        }
+        if (remainder >= q) {
+            cq = Math.floor(remainder / q);
+            result["Q"] = cq;
+            q *= cq;
+            remainder -= q;
+        }
+        if (remainder >= d) {
+            cd = Math.floor(remainder / d);
+            result["D"] = cd;
+            d *= cd;
+            remainder -= d;
+        }
+        if (remainder >= n) {
+            cn = Math.floor(remainder / n);
+            result["N"] = cn;
+            n *= cn;
+            remainder -= n;
+        }
+        if (remainder >= p) {
+            cp = Math.floor(remainder / p);
+            result["P"] = cp;
+        }
+
+        return result;
+    }
 }
